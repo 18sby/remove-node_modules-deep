4426a2c6d56f317ac6c38d235226b19186b1fa44
songboyan <songboyan@yidian-inc.com>
2020-07-22 07:17:26 +0800

    发布npm包完成

--- bin/remove.js
+++ bin/remove.js
@@ -1,16 +1,24 @@
+#! /usr/bin/env node
+
 /*
   递归删除所有的 node_moduels 文件夹
 */
 
+const program = require('commander');
 const shell = require('shelljs'); // 在 node 中运行 windows/linux/mac 命令
 const path = require('path');
 const fs = require('fs');
 
 const moduleReg = /node_modules/; // 正则匹配所有 node 依赖文件夹
 let count = 0; // 统计删除的 node_modules 文件夹数量
-const deleteDirs = []; // 删除的目录路径
+// const deleteDirs = []; // 删除的目录路径
 const DEFAULT_FILEPATH = process.cwd(); // 当前工作目录
-// console.log( '当前工作目录', defaultFilepath );
+
+program.option('--path <v>', 'set relative path[default is current work directory]');
+
+const { path: filepath } = program.parse(process.argv);
+removeModules(filepath);
+console.log('已删除 node_modules 文件夹 ' + count + ' 个');
 
 /**
  * todo 递归删除当前目录内部的所有 node_modules 文件夹
@@ -21,7 +29,7 @@ function removeModules(filepath = DEFAULT_FILEPATH) {
     let statObj = fs.statSync(filepath);
     if (statObj.isDirectory()) { // 是文件
       if (moduleReg.test(filepath)) {
-        deleteDirs.push(filepath);
+        // deleteDirs.push(filepath);
         count++;
         shell.rm('-rf', filepath);
         return ;
@@ -35,7 +43,3 @@ function removeModules(filepath = DEFAULT_FILEPATH) {
     throw new Error('文件路径无效: ', err);
   }
 }
-
-removeModules(path.resolve(__dirname, '../../sby'));
-console.log('已删除 node_modules 文件夹 ' + count + ' 个');
-console.log('删除的目录有：', deleteDirs);
